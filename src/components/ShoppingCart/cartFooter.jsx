4eaad2eba60bfb871c1e824ba62459e9c6bbc025
carpediem511 <vampire.0514@gmail.com>
2023-11-28 16:14:18 +0300

    //

--- src/components/ShoppingCart/cartFooter.jsx
+++ src/components/ShoppingCart/cartFooter.jsx
@@ -1,22 +1,22 @@
 const CartFooter = ({ total, showFooter }) => {
-  if (!showFooter) {
-    return null;
-  }
+	if (!showFooter) {
+		return null;
+	}
 
-  const { count, cost } = total;
+	const { count, cost } = total;
 
-  let priceFormatNew = new Intl.NumberFormat();
+	let priceFormatNew = new Intl.NumberFormat();
 
-  return (
-    <div className="flex bg-teal-50 py-4 text-emerald-600 mb-6 text-start">
-      <div className="w-1/6"></div>
-      <div className="w-2/6"></div>
-      <div className="w-1/6">{count} шт.</div>
-      <div className="w-1/6 text-sm sm:text-md sm:font-semibold lg:text-lg">
-        {priceFormatNew.format(cost)} рублей
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex bg-teal-50 py-4 text-emerald-600 mb-6 text-start">
+			<div className="w-1/6"></div>
+			<div className="w-2/6"></div>
+			<div className="w-1/6">{count} шт.</div>
+			<div className="w-1/4 text-sm sm:text-md sm:font-semibold lg:text-base">
+				{priceFormatNew.format(cost)} рублей
+			</div>
+		</div>
+	);
 };
 
 export default CartFooter;
