aa4ec5bcd3f6553de8b20b80b707709457704860
carpediem511 <vampire.0514@gmail.com>
2023-11-28 17:48:39 +0300

    //

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -5,43 +5,43 @@ import SuccessModal from "./SuccessModal";
 import { useState } from "react";
 
 const Modal = ({ active, setActive, productList, setProductList }) => {
-	const [orderPlaced, setOrderPlaced] = useState(false);
+  const [orderPlaced, setOrderPlaced] = useState(false);
 
-	const isCartEmpty = productList.filter((p) => p.isAdded).length === 0;
+  const isCartEmpty = productList.filter((p) => p.isAdded).length === 0;
 
-	const handleButtonClick = () => {
-		setOrderPlaced(true);
-		setActive(false);
-	};
+  const handleButtonClick = () => {
+    setOrderPlaced(true);
+    setActive(false);
+  };
 
-	const closeModal = () => {
-		setOrderPlaced(false);
-		setActive(false);
-	};
+  const closeModal = () => {
+    setOrderPlaced(false);
+    setActive(false);
+  };
 
-	return (
-		<>
-			{active && (
-				<div className="modal fixed inset-0 flex items-center justify-center bg-opacity-75 bg-gray-500">
-					<div
-						className="modal-content bg-white w-full max-w-2xl p-4 rounded-lg shadow-lg"
-						onClick={(e) => e.stopPropagation()}
-					>
-						<Cart productList={productList} setProductList={setProductList} />
+  return (
+    <>
+      {active && (
+        <div className="modal fixed inset-0 flex items-center justify-center bg-opacity-75 bg-gray-500">
+          <div
+            className="modal-content bg-white w-full max-w-2xl p-4 rounded-lg shadow-lg"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <Cart productList={productList} setProductList={setProductList} />
 
-						<div className="flex justify-center space-x-4 mt-4">
-							{!isCartEmpty && <ButtonSend onClick={handleButtonClick} />}
-							<ButtonOpenClose
-								handleClick={() => setActive(false)}
-								title="Закрыть"
-							/>
-						</div>
-					</div>
-				</div>
-			)}
-			{orderPlaced && <SuccessModal closeModal={closeModal} />}
-		</>
-	);
+            <div className="flex justify-center space-x-4 mt-4">
+              {!isCartEmpty && <ButtonSend onClick={handleButtonClick} />}
+              <ButtonOpenClose
+                handleClick={() => setActive(false)}
+                title="Закрыть"
+              />
+            </div>
+          </div>
+        </div>
+      )}
+      {orderPlaced && <SuccessModal closeModal={closeModal} />}
+    </>
+  );
 };
 
 export default Modal;
